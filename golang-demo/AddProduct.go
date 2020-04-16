43849a704a6fd2c25eb413b9871c28a8f56ef403
wanghj321 <448120231@qq.com>
2020-04-16 17:12:33 +0800

    Add files via upload
    a

---
+++ golang-demo/AddProduct.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+    "crypto/hmac"
+    "crypto/sha256"
+    "encoding/hex"
+    "fmt"
+    "time"
+)
+
+func sha256hex(s string) string {
+    b := sha256.Sum256([]byte(s))
+    return hex.EncodeToString(b[:])
+}
+
+func hmacsha256(s, key string) string {
+    hashed := hmac.New(sha256.New, []byte(key))
+    hashed.Write([]byte(s))
+    return string(hashed.Sum(nil))
+}
+
+func main() {
+    secretId := "AKIDz8krbsJ5yKBZQpn74WFkmLPx3EXAMPLE"
+    secretKey := "Gu5t9xGARNpq86cd98joQYCN3EXAMPLE"
+    host := "yunxiaowei.qcloud.com"
+    algorithm := "HMAC-SHA256"
+    service := "industry"
+    version := "v1"
+    action := "AddProduct"
+   
+    var timestamp int64 = time.Now().Unix()
+    //var timestamp int64 = 1551113065
+
+    // step 1: build canonical request string
+    httpRequestMethod := "POST"
+    canonicalURI := "/"
+    canonicalQueryString := ""
+    canonicalHeaders := "content-type:application/json; charset=utf-8\n"
+    signedHeaders := "content-type"
+    payload := `{"remark": "", "product_name": "test1"}`
+    hashedRequestPayload := sha256hex(payload)
+    canonicalRequest := fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n%s",
+        httpRequestMethod,
+        canonicalURI,
+        canonicalQueryString,
+        canonicalHeaders,
+        signedHeaders,
+        hashedRequestPayload)
+    fmt.Println(canonicalRequest)
+
+    // step 2: build string to sign
+    date := time.Unix(timestamp, 0).UTC().Format("2006-01-02")
+    credentialScope := fmt.Sprintf("%s/%s/tc3_request", date, service)
+    hashedCanonicalRequest := sha256hex(canonicalRequest)
+    string2sign := fmt.Sprintf("%s\n%d\n%s\n%s",
+        algorithm,
+        timestamp,
+        credentialScope,
+        hashedCanonicalRequest)
+    fmt.Println(string2sign)
+
+    // step 3: sign string
+    secretDate := hmacsha256(date, "TC3"+secretKey)
+    secretService := hmacsha256(service, secretDate)
+    secretSigning := hmacsha256("tc3_request", secretService)
+    signature := hex.EncodeToString([]byte(hmacsha256(string2sign, secretSigning)))
+    fmt.Println(signature)
+
+    // step 4: build authorization
+    authorization := fmt.Sprintf("%s Credential=%s/%s, SignedHeaders=%s, Signature=%s",
+        algorithm,
+        secretId,
+        credentialScope,
+        signedHeaders,
+        signature)
+    fmt.Println(authorization)
+
+    curl := fmt.Sprintf(`curl -X POST https://%s\
+ -H "Authorization: %s"\
+ -H "Content-Type: application/json; charset=utf-8"\
+ -H "YXW-Action: %s"\
+ -H "YXW-Timestamp: %d"\
+ -H "YXW-Version: %s"\
+ -d '%s'`, host+canonicalURI, authorization, action, timestamp, version, payload)
+    fmt.Println(curl)
+}
